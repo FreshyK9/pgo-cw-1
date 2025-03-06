458c510db2b5c5e98fbc27b2b8eeac678b596adc
Arsenij Arsiriy <s34679@pjwstk.edu.pl>
2025-03-06 09:34:13 +0100

    Main2

--- Glizz/src/Main.java
+++ Glizz/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int a = 15;
+        int b = 40;
+
+        System.out.println(" what is up bros" + " " + a);  // Print a
+        System.out.println(b);  // Print b
     }
 }
